f809191a550bb95294d71b6269487cb80270d3a1
Marcin <31660690+msemczuk@users.noreply.github.com>
2025-02-08 23:22:21 +0100

    dot

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,7 +37,12 @@ const Index = () => {
           <p className="text-neutral-600 leading-relaxed">
             Welcome to my website! I am an astrophysicist interested in galactic dynamics and numerical simulations.
             Currently I am a postdoc in the GaiaUB group at the Institut de Ciències del Cosmos - Universitat de Barcelona.
-            You will find my publications in this 
+            Check my 
+<<<<<<< HEAD
+            <a href="https://ui.adsabs.harvard.edu/search/q=docs(library%2FRO7hi4tGRIO3jE0VTM-gDA)&sort=date%20desc%2C%20bibcode%20desc&p_=0" target="_blank" rel="noopener noreferrer">
+             ADS library.</a>
+            
+=======
             <a 
               href="https://ui.adsabs.harvard.edu/search/q=docs(library%2FRO7hi4tGRIO3jE0VTM-gDA)&sort=date%20desc%2C%20bibcode%20desc&p_=0" 
               target="_blank" 
@@ -45,7 +50,8 @@ const Index = () => {
               className="inline-flex items-center mx-1 text-primary hover:text-primary-dark underline decoration-2 decoration-primary/30 hover:decoration-primary transition-colors"
             >
               ADS library
-            </a>.
+            </a>
+>>>>>>> d661651e778a18053a151d2ccf959a51253a1289
           </p>
         </div>
       </section>
